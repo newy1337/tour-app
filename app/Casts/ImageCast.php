9962c8adb2589bf706274f1535a876e8f1804118
NEWY-PC <newy1337@gmail.com>
2025-03-12 15:40:48 +0000

    proxie trust

---
+++ app/Casts/ImageCast.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class ImageCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        $filenameWithoutExt = pathinfo($value, PATHINFO_FILENAME);
+        $extension = pathinfo($value, PATHINFO_EXTENSION);
+        $baseUrl = url('/');
+        return "{$baseUrl}/storage/tours/{$filenameWithoutExt}.{$extension}";
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return $value;
+    }
+}
