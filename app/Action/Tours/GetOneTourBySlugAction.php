cbf0788223d593e87872af2356641ab46669e89e
NEWY-PC <newy1337@gmail.com>
2025-03-18 15:14:24 +0000

    tours updated

--- app/Action/Tours/GetOneTourBySlugAction.php
+++ app/Action/Tours/GetOneTourBySlugAction.php
@@ -17,7 +17,7 @@ class GetOneTourBySlugAction
         $filenameWithoutExt = pathinfo($pathOriginal, PATHINFO_FILENAME);
         $extension = pathinfo($pathOriginal, PATHINFO_EXTENSION);
 
-        $tour->header_image = "{$filenameWithoutExt}_1400x470.{$extension}";
+        $tour->header_image = "tours/{$filenameWithoutExt}_1400x470.{$extension}";
 
         $tour->related = Tour::inRandomOrder()
             ->where('hidden',false)
