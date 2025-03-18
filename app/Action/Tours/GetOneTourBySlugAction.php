d088487016241a03c55c0fb0099d872a871f4b3d
NEWY-PC <newy1337@gmail.com>
2025-03-18 15:06:52 +0000

    related

--- app/Action/Tours/GetOneTourBySlugAction.php
+++ app/Action/Tours/GetOneTourBySlugAction.php
@@ -18,9 +18,31 @@ class GetOneTourBySlugAction
         $extension = pathinfo($pathOriginal, PATHINFO_EXTENSION);
 
         $tour->header_image = "{$filenameWithoutExt}_1400x470.{$extension}";
+
+        $tour->related = Tour::inRandomOrder()
+            ->where('hidden',false)
+            ->take(6)
+            ->get($this->returnModel())
+            ->map(function ($tour) {
+                $tour->header_image = $this->resizeImage($tour->header_image);
+                return $tour;
+            });
         return $tour;
 
 
+    }
+
+    private function resizeImage($image): string
+    {
+        $filenameWithoutExt = pathinfo($image, PATHINFO_FILENAME);
+        $extension = pathinfo($image, PATHINFO_EXTENSION);
+        return "tours/{$filenameWithoutExt}_800x533.{$extension}";
+
+    }
+
+    private function returnModel(): array
+    {
+        return ['*'];
     }
 
 }
